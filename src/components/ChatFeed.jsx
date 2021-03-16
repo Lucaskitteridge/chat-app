e35cdbda201f2a13f3716f5ea182575e2ba7641d
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-16 13:58:19 -0400

    Chatfeed component finished. working on other message components

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,9 +1,50 @@
+import MyMessage from './MyMessage'
+import MessageForm from './MessageForm'
+import TheirMessage from './TheirMessage'
+
 const ChatFeed = (props) => {
-  console.log(props)
+
+  const { chats, activeChat, userName, messages } = props
+
+  const chat = chats && chats[activeChat]
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages)
+
+    return keys.map((key, index) => {
+      const message = messages[key]
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === MessageEvent.sender.userName;
+
+      return (
+        <div key={`message_${index}`} style={{ width: '100%' }}>
+          <div className='messageblock'>
+            {isMyMessage ? <MyMessage message={message}/> : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />}
+          </div>
+          <div className='readreceipt' style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  if (!chat) return 'Loading...'
 
   return (
-    <div>
-      ChatFeed
+    <div className="chatFeed">
+      <div className="chatTitleContainer">
+        <div className='chatTitle'>
+          {chat?.title}
+          <div className='subtitle'>
+            {chat.people.map((person) => ` ${person.person.userName}`) }
+          </div>
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{height:'100px'}} />
+      <div className='messageFromContainer'>
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
     </div>
   )
 }
---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,9 @@
+const MessageForm = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MessageForm
