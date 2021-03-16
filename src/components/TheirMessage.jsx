7165310b030d4b828295a483d0944e6e892e676f
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-16 15:12:56 -0400

    their message logic added

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,28 @@
-const TheirMessage = (props) => {
+const TheirMessage = ({ message, lastMessage }) => {
+
+  const isFirstMessage = !lastMessage || lastMessage.sender.userName !== message.sender.userName
   return (
-    <div>
-      TheirMessage 
+    <div className='messageRoom'>
+      {isFirstMessage && (
+        <div
+          className="messageAvatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments.length > 0
+        ? (
+          <img
+            src={message.attachments[0].file}
+            alt='message_attachment'
+            className='messageimg'
+            styles={{ marginLeft: isFirstMessage ? '4px' : '48px' }}
+          />
+        ) : (
+          <div className='message' style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessage ? '4px' : '48px'  }}>
+            {message.text}
+          </div>
+        )
+      }
     </div>
   )
 }
