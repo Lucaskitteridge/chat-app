ae09e2ecc1268b55684119ef8e3dce043d74aa08
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-17 11:11:56 -0400

    photo upload added

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import {sendMessage, isTyping} from 'react-chat-engine'
+import { SendOutlined, PictureOutlined } from '@ant-design/icons'
 
 const MessageForm = (props) => {
 
@@ -16,6 +17,10 @@ const MessageForm = (props) => {
     }
   }
 
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, {files: event.target.files, text: ''})
+  }
+
   const handleChange = (event) => {
     setValue(event.target.value)
 
@@ -25,7 +30,15 @@ const MessageForm = (props) => {
   return (
     <form className='message-form' onSubmit={handleSubmit}>
       <input className='message-input' placeholder='Send a message...' value={value} onChange={handleChange} onSubmit={handleSubmit}/>
-
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className='picture-icon'/>
+        </span>
+      </label>
+      <input typeof='file' multiple={false} id='upload-button' style={{display:'none'}} onChange={handleUpload}/>
+      <button type="submit" className="send-button">
+        <SendOutlined  className='send-icon'/>
+      </button>
     </form>
   )
 }
