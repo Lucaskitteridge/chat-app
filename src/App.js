9418c8218e1ec1960fb4239700392a78b05176dc
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-17 12:15:41 -0400

    login feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
 
-import './App.css'
-import ChatFeed from './components/ChatFeed'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
 
-const app = () => {
+const projectID = 'ec70c76a-83d6-4f89-ba2f-292254f009ef';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
-      height='100vh'
-      projectID='ec70c76a-83d6-4f89-ba2f-292254f009ef'
-      userName='Lucas'
-      userSecret='123123'
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-  )
-}
+  );
+};
 
-export default app
+export default App;
